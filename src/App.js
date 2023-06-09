78976180551d83f2b1b24eebc1376d3ebc4420c2
petiand <peti_andor@yahoo.com>
2023-06-09 15:18:13 +0300

    commiting from the D directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={require("./csonka.jpg")} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SALONTA APP
         </a>
       </header>
     </div>
